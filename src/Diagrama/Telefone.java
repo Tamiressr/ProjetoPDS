4d9d831c1705a2c4838d553a60b7d647ced376ef
Antoniojustino4 <32883667+Antoniojustino4@users.noreply.github.com>
2018-05-15 18:18:25 -0300

    Revert "Teste"
    This reverts commit 9a8738d3bdfa8ce1b11b9836e3f37350de618dfe.

--- src/Diagrama/Telefone.java
+++ src/Diagrama/Telefone.java
@@ -1,26 +1,20 @@
 <<<<<<< HEAD:src/Model/Telefone.java
 package Model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 =======
 package Diagrama;
 >>>>>>> b1c2f618abb6de4baf0b0a4733e0a706c7b191fe:src/Diagrama/Telefone.java
 
-@Entity
 public class Telefone {
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id;
-	
 	private short ddd;
+<<<<<<< HEAD:src/Diagrama/Telefone.java
 
 <<<<<<< HEAD:src/Model/Telefone.java
+=======
+>>>>>>> parent of 9a8738d... Teste:src/Model/Telefone.java
 	private String numero;
-	
+
 	@ManyToOne
 	private Escola escola;
 =======
@@ -33,9 +27,7 @@ public class Telefone {
 	
 >>>>>>> b1c2f618abb6de4baf0b0a4733e0a706c7b191fe:src/Diagrama/Telefone.java
 	
-	public Escola getEscola() {
-		return escola;
-	}
+	
 	
 	public String toString() {
 		return ddd+numero;
